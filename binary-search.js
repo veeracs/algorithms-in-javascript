e6acfdf80db43fdeb33c7efa52f67e3137c6e173
Chandra Sekhar Veera <veeracs@icloud.com>
2015-07-08 19:55:38 -0400

    binary search

---
+++ binary-search.js
@@ -0,0 +1,37 @@
+//  In worst case, a binary search on an array of 100,000 items performs a maximum of 16 comparisons
+//  while a linear search within the same array performs a maximum of 100,000 comparisons.
+
+Array.prototype.binaryIndexOf = Array.prototype.binaryIndexOf || function binaryIndexOf(value) {
+  var firstIndex = 0,
+      lastIndex = this.length-1,
+      middleIndex = Math.floor((firstIndex+lastIndex)/2);
+  while (value !== this[middleIndex] && firstIndex < lastIndex) {
+    if (value < this[middleIndex]) {
+      lastIndex = middleIndex-1;
+    } else {
+      firstIndex = middleIndex+1;
+    }
+    //  reset the middleIndex
+    middleIndex = Math.floor((firstIndex + lastIndex)/2);
+  }
+  //  end of searching the array
+  return (this[middleIndex] !== value) ? -1 : middleIndex;
+};
+//  sample execution of binaryIndexOf
+var animals = [
+  {type: "dog", age: 2},
+  {type: "dog", age: 32},
+  {type: "dog", age: 12},
+  {type: "dog", age: 21},
+  {type: "dog", age: 3},
+  {type: "dog", age: 20},
+  {type: "dog", age: 22}
+];
+
+animals.sort(function(a, b) {
+  return (a.age - b.age);
+});
+
+console.log(animals.map(function(animal) {
+  return animal.age;
+}).binaryIndexOf(20));
