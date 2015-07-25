92f5effdcf9bfb2eca6aee01ea559034ceacc76e
Chandra Sekhar Veera <veeracs@icloud.com>
2015-07-25 12:04:06 -0400

    add stack class

--- stack.js
+++ stack.js
@@ -1,7 +1,29 @@
 //	Stack - LIFO
 function Stack() {
 	var items = [];
-	this.unshift = function() {
-
+	this.add = function(item) {
+        items.unshift(item);
+		return this;
 	};
+	this.remove = function() {
+        items.shift();
+      	return this;
+	};
+    this.get = function() {
+      return items;
+    };
+	return this;
+}
+
+var mystack = new Stack();
+mystack.add(10).add(12);
+mystack.remove();
+
+console.log(mystack.get());
+
+//	Queue - FIFO
+
+function Queue() {
+	var items = [];
+
 }
