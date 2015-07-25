527f284fcdf3cf4bd3325a0ec3ecc48b99a4d407
Chandra Sekhar Veera <veeracs@icloud.com>
2015-07-25 13:26:43 -0400

    add queue

--- stack.js
+++ stack.js
@@ -15,9 +15,9 @@ function Stack() {
 	return this;
 }
 
-var mystack = new Stack();
-mystack.add(10).add(12);
-mystack.remove();
+var myStack = new Stack();
+myStack.add(10).add(12);
+myStack.remove();
 
 console.log(mystack.get());
 
@@ -25,5 +25,21 @@ console.log(mystack.get());
 
 function Queue() {
 	var items = [];
-
+	this.add = function(item) {
+		items.push(item);
+		return this;
+	};
+	this.remove = function() {
+		items.shift();
+		return this;
+	};
+	this.get = function() {
+		return items;
+	}
+	return this;
 }
+
+var myQueue = new Queue();
+myQueue.add(100).add(23).add(12).add(78);
+myQueue.remove();
+console.log(myQueue.get());
