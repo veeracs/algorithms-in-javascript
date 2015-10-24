9d61a110d04d7eb60e6fa4c434bd1ba2ccdc9874
Chandra Sekhar Veera <veeracs@icloud.com>
2015-10-24 11:31:22 -0400

    add comments

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -8,25 +8,32 @@ function BinarySearchTree() {
 BinarySearchTree.prototype = {
     constructor: BinarySearchTree,
     insert: function insert(value, currNode) {
+        //  initialize the node to be inserted
         var node = {
             value: value,
             left: null,
             right: null
         };
+        //  set the root node of the tree
         if (!this.root) {
             this.root = node;
             return this;
         }
-        var insertKey = "right";
+        //  currNode helps recursively navigate the tree for node insertion
         currNode = currNode || this.root;
+        //  determine where to insert the node
+        var insertKey = "right";
         if (value < currNode.value) {
             insertKey = "left";
         }
+        //  insert the node
         if (!currNode[insertKey]) {
             currNode[insertKey] = node;
         } else {
+            //  recursively call insert
             this.insert(value, currNode[insertKey]);
         }
+
         return this;
     }
 };
@@ -34,8 +41,9 @@ BinarySearchTree.prototype = {
 var btree = new BinarySearchTree();
 btree.insert(100).insert(20).insert(120).insert(12);
 
-//  Level-order traversal is breadth first traversal for the
+//  Level-order traversal is breadth first traversal
 //  For each node, first the node is visited and then it's child nodes are put in FIFO
+
 var queue = [];
 var tempNode = btree.root;
 
